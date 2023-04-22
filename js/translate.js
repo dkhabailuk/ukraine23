31b40c5d7a7c721b19abe22f852a5dc13699f8f0
dkhabailuk <95091427+dkhabailuk@users.noreply.github.com>
2023-04-22 08:42:42 +0100

    Add files via upload

---
+++ js/translate.js
@@ -0,0 +1,27 @@
+var translate = {
+    ua: {
+      m1: "Żaden wiatr nie jest dobry dla okrętu, który nie zna portu swego przeznaczenia.",
+      m2: "Konfucjusz",
+    },
+    en: {
+      m1: "No wind is favourable for a ship that doesn't know its destiny harbor.",
+      m2: "Confucius",
+    },
+  };
+  
+  class translator {
+    constructor() {
+      this.changeLanguage();
+    }
+    changeLanguage() {
+      var i18n = translate;
+      $(".language select").change(function() {
+        var lang = $(this).val();
+        $("#atr1").text(i18n[lang].m1, 500);
+        $("#atr2").text(i18n[lang].m2, 500);
+      });
+    }
+   
+  }
+  
+  new translator()
